15e0e0fa7ab718b9353944cf1e726a48304eb8d7
Pete Russell <pinkbatz@gmail.com>
2017-09-18 14:04:16 -0500

    Adds ingredients list and Add form.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,7 +11,10 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('Test Recipe', 'This is just a test recipe',
       'http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-fava-' +
-      'carrot-ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg')
+      'carrot-ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg'),
+      new Recipe('Test Recipe 2', 'This is just another test recipe',
+        'http://www.seriouseats.com/recipes/assets_c/2016/03/20160324-aquafaba' +
+        '-pancake-oatmeal-vegan-21-thumb-1500xauto-430826.jpg')
   ];
 
   constructor() { }
---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,12 @@
+export class Ingredient {
+  // The following TypeScript is equivalent to the code below.
+  constructor(public name: string, public amount: number) {}
+  
+  // public name: string;
+  // public amount: number;
+  //
+  // constructor(name: string, amount: number) {
+  //   this.name = name;
+  //   this.amount = amount;
+  // }
+}
