35e7386e172034ee4bb51d40cc52526dd1a48ec1
Pete Russell <pinkbatz@gmail.com>
2017-09-26 16:27:35 -0700

    Adds ingredients list to recipe details.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,17 +1,32 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
-  
+
   private recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'This is just a test recipe',
+    new Recipe(
+      'Tasty schnitzel',
+      'A super tasty schnitzel, just awesome!',
       'http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-fava-' +
-      'carrot-ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg'),
-      new Recipe('Test Recipe 2', 'This is just another test recipe',
-        'http://www.seriouseats.com/recipes/assets_c/2016/03/20160324-aquafaba' +
-        '-pancake-oatmeal-vegan-21-thumb-1500xauto-430826.jpg')
+      'carrot-ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]
+    ),
+    new Recipe(
+      'Burger',
+      'Can\'t go wrong with this classic',
+      'http://www.seriouseats.com/recipes/assets_c/2016/03/20160324-aquafaba' +
+      '-pancake-oatmeal-vegan-21-thumb-1500xauto-430826.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ]
+    )
   ];
 
   getRecipes() {
